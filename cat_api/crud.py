ca3ac0c4dceab003d416c6d0ff5bd1ccfbf9ba71
evaristofm <evaristofm@gmail.com>
2021-02-21 17:28:49 -0300

    Finalizando roteas, crud e adicionado filter

--- cat_api/crud.py
+++ cat_api/crud.py
@@ -3,6 +3,23 @@ from sqlalchemy.orm import Session
 from . import models, schemas
 
 
+def get_cats_with_filter(
+    db: Session, breed: str = None, location: str = None,
+    coat: str = None, body: str = None, pattern: str = None
+):
+    if breed:
+        return db.query(models.Cat).filter(models.Cat.breed == breed).all()
+    elif location:
+        return db.query(models.Cat).filter(models.Cat.location_origin == location).all()
+    elif coat:
+        return db.query(models.Cat).filter(models.Cat.coat_length == coat).all()
+    elif body:
+        return db.query(models.Cat).filter(models.Cat.body_type == body).all()
+    elif pattern:
+        return db.query(models.Cat).filter(models.Cat.pattern == pattern).all()
+    return db.query(models.Cat).all()
+
+
 def get_cat(db: Session, cat_id: int):
     return db.query(models.Cat).filter(models.Cat.id == cat_id).first()
 
@@ -16,3 +33,20 @@ def create_cat(db: Session, cat: schemas.Cat):
     db.refresh(cat_db)
     return cat_db
 
+def update_cat(db: Session, id: int, cat: schemas.Cat):
+    cat_db = get_cat(db=db, cat_id=id)
+
+    cat_db.breed = cat.breed if cat.breed != 'string' else cat_db.breed
+    cat_db.location_origin = cat.location_origin if cat.location_origin != 'string' else cat_db.location_origin
+    cat_db.coat_length = cat.coat_length if cat.coat_length != 'string' else cat_db.coat_length
+    cat_db.body_type = cat.body_type if cat.body_type != 'string' else cat_db.body_type
+    cat_db.pattern = cat.pattern if cat.pattern != 'string' else cat_db.pattern
+
+    db.commit()
+    db.refresh(cat_db)
+    return cat_db
+
+def delete_cat(db: Session, id: int):
+    db.query(models.Cat).filter(models.Cat.id == id).delete()
+    db.commit()
+    
--- cat_api/main.py
+++ cat_api/main.py
@@ -18,24 +18,39 @@ def get_db():
     finally:
         db.close()
 
-some_dict = dict(
-    path1="AcceptedEventRelation",
-    path2="Account",
-    path3="AccountChangeEvent",
-    path4="AccountCleanInfo",
-    path5="AccountContactRole"
-)
 
-Subjects = Enum('Subjects', some_dict)
-
+@app.get("/cats/", response_model=List[schemas.Cat])
+def list_cats(breed: str = None, location: str = None, coat: str = None, body: str = None, pattern: str = None, db: Session = Depends(get_db)):
+    cats = crud.get_cats_with_filter(db=db, breed=breed, location=location, coat=coat, body=body, pattern=pattern)
+    return cats
 
 @app.get("/cats", response_model=List[schemas.Cat])
-def cats_list(skip: int = 0, limit: int = 20, db: Session = Depends(get_db)):
-    cat_db = crud.get_cats(db=db, skip=skip, limit=limit)
-    return cat_db
+def cats_all(skip: int = 0, limit: int = 20, db: Session = Depends(get_db)):
+    cats_db = crud.get_cats(db=db, skip=skip, limit=limit)
+    return cats_db
+
+@app.get("/cats/{id}", response_model=schemas.Cat)
+def cats_one(id: int, db: Session = Depends(get_db)):
+   cat = crud.get_cat(db=db, cat_id=id)
+   if cat is None:
+       raise HTTPException(404, detail="Cat Not Found")
+   return cat
 
 @app.post("/cats", response_model=schemas.Cat)
 def cat_create(cat: schemas.CatIn, db: Session = Depends(get_db)):
     return crud.create_cat(db=db, cat=cat)
 
- 
+@app.put("/cats/{id}", response_model=schemas.Cat)
+def update_cat(cat: schemas.CatIn, id: int, db: Session = Depends(get_db)):
+    cat = crud.update_cat(db=db, id=id, cat=cat)
+    if cat is None:
+        raise HTTPException(404, detail="Cat Not Found.")
+    return cat
+
+@app.delete("/cats/{id}")
+def delete_cat(id: int, db: Session = Depends(get_db)):
+    cat = crud.get_cat(db=db, cat_id=id)
+    if cat:
+        crud.delete_cat(db=db, id=id)
+        return {"message": "Cat Deleted"}
+    raise HTTPException(404, detail="Cat Not Found")
